6959d0163a9fafd7e63313b6e4fb1453ff7944df
EC2 Default User <ec2-user@ip-172-31-29-187.ap-northeast-1.compute.internal>
2022-12-19 09:06:34 +0000

    Update layouts

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def top
-    @customers = Customer.page(params[:page]).per(10)
+    @customers = Customer.where.not(name: 'guestuser').page(params[:page]).per(10)
   end
 end
